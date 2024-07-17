caeb938755c2322c8e967c76b1f29f5e81cf09c5
varsanihemal <hemalvarsani123@gmail.com>
2024-07-17 16:22:39 -0500

    generating the models

---
+++ db/migrate/20240717211215_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[7.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.decimal :price
+      t.integer :stock_quantity
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end

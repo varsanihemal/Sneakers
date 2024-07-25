07a32723339d488778d194f8474152ccfb62681c
varsanihemal <hemalvarsani123@gmail.com>
2024-07-25 01:07:39 -0500

    making the cart display function

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,7 +1,10 @@
-class CartController < ApplicationController
+# app/controllers/carts_controller.rb
+class CartsController < ApplicationController
   before_action :authenticate_user! # Ensure user is signed in to view cart
 
   def show
-    @cart = current_user.cart
+    @cart = current_user.carts.first
+    # Or, if you have a way to mark an active cart:
+    # @cart = current_user.carts.find_by(active: true)
   end
 end

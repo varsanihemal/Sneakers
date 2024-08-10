1189147e4e5dc04834f3ac3fb972f78c02c85b91
varsanihemal <hemalvarsani123@gmail.com>
2024-08-09 23:25:40 -0500

    migration error fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_08_10_024210) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
     t.string "resource_type"
-    t.integer "resource_id"
+    t.bigint "resource_id"
     t.string "author_type"
-    t.integer "author_id"
+    t.bigint "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author"
@@ -66,8 +69,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_10_024210) do
   end
 
   create_table "cart_items", force: :cascade do |t|
-    t.integer "cart_id", null: false
-    t.integer "product_id", null: false
+    t.bigint "cart_id", null: false
+    t.bigint "product_id", null: false
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -76,7 +79,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_10_024210) do
   end
 
   create_table "carts", force: :cascade do |t|
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_carts_on_user_id"
@@ -89,8 +92,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_10_024210) do
   end
 
   create_table "order_items", force: :cascade do |t|
-    t.integer "order_id", null: false
-    t.integer "product_id", null: false
+    t.bigint "order_id", null: false
+    t.bigint "product_id", null: false
     t.integer "quantity"
     t.decimal "price_at_purchase"
     t.datetime "created_at", null: false
@@ -100,7 +103,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_10_024210) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.decimal "tax_amount", precision: 10, scale: 2
     t.string "status"
     t.datetime "created_at", null: false
@@ -118,13 +121,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_10_024210) do
     t.string "payment_status"
     t.string "stripe_charge_id"
     t.string "payment_intent_id"
-    t.integer "total_amount"
+    t.integer "total_amount", default: 0
     t.string "client_secret"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "product_images", force: :cascade do |t|
-    t.integer "product_id", null: false
+    t.bigint "product_id", null: false
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -135,7 +138,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_10_024210) do
     t.string "name"
     t.text "description"
     t.decimal "price", precision: 10, scale: 2
-    t.integer "category_id"
+    t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_products_on_category_id"
@@ -151,8 +154,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_10_024210) do
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.integer "product_id", null: false
-    t.integer "user_id", null: false
+    t.bigint "product_id", null: false
+    t.bigint "user_id", null: false
     t.integer "rating"
     t.text "comment"
     t.datetime "created_at", null: false
@@ -178,7 +181,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_10_024210) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer "province_id"
+    t.bigint "province_id"
     t.string "address"
     t.string "city"
     t.string "postal_code"
